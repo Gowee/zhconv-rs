08267df391dc34fc34215251a2933a75e078853e
Hung-I Wang <whygowe@gmail.com>
2021-12-04 15:34:16 +0800

    chore: add benchmark "load all"

--- benches/zhconv_benchmark.rs
+++ benches/zhconv_benchmark.rs
@@ -2,21 +2,39 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use zhconv::{convs::*, ZhConverter};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("load zh2Hant", |b| b.iter(|| make_converter(black_box(ZH_HANT_CONV))));
-    c.bench_function("load zh2Hans", |b| b.iter(|| make_converter(black_box(ZH_HANS_CONV))));
-    c.bench_function("load zh2TW", |b| b.iter(|| make_converter(black_box(ZH_TW_CONV))));
-    c.bench_function("load zh2HK", |b| b.iter(|| make_converter(black_box(ZH_HK_CONV))));
-    c.bench_function("load zh2CN", |b| b.iter(|| make_converter(black_box(ZH_CN_CONV))));
+    c.bench_function("load zh2Hant", |b| {
+        b.iter_with_large_drop(|| make_converter(black_box(ZH_HANT_CONV)))
+    });
+    c.bench_function("load zh2Hans", |b| {
+        b.iter_with_large_drop(|| make_converter(black_box(ZH_HANS_CONV)))
+    });
+    c.bench_function("load zh2TW", |b| {
+        b.iter_with_large_drop(|| make_converter(black_box(ZH_TW_CONV)))
+    });
+    c.bench_function("load zh2HK", |b| {
+        b.iter_with_large_drop(|| make_converter(black_box(ZH_HK_CONV)))
+    });
+    c.bench_function("load zh2CN", |b| {
+        b.iter_with_large_drop(|| make_converter(black_box(ZH_CN_CONV)))
+    });
+    c.bench_function("load all", |b| {
+        b.iter_with_large_drop(|| {
+            (
+                make_converter(black_box(ZH_HANS_CONV)),
+                make_converter(black_box(ZH_HANT_CONV)),
+                make_converter(black_box(ZH_CN_CONV)),
+                make_converter(black_box(ZH_HK_CONV)),
+                make_converter(black_box(ZH_TW_CONV)),
+            )
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
+// criterion_group! {
+//     name = benches;
+//     config = Criterion::default().sample_size(500);
+//     targets = criterion_benchmark
+// }
 criterion_main!(benches);
