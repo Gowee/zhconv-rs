a9f68baeba2559823e5694ba5f577d42c7fb260f
Hung-I Wang <whygowe@gmail.com>
2022-08-11 23:19:40 +0800

    feat(pyo3): always release gil
    Previously, releasing gil is provided as a seprate function due to
    performance overhead incurred by FFI calls. Now after tests, the
    overheaed is trivial. For "天干物燥" to zh-TW, the measurable time
    difference is just less than 200ns.

--- pyo3/src/lib.rs
+++ pyo3/src/lib.rs
@@ -1,39 +1,32 @@
 use std::str::FromStr;
 
+use ::zhconv::{zhconv as zhconv_plain, zhconv_mw, Variant};
 use pyo3::prelude::*;
-use ::zhconv::{Variant, zhconv as zhconv_plain, zhconv_mw};
 
-/// Formats the sum of two numbers as string.
+/// zhconv(text, target[, mediawiki]) -> result
+/// 
+/// Convert the text to a target Chinese variant. Converters are constructed from built-in rulesets
+/// on demand and cached automatically. If `mediawiki` is `True`, inline conversion rules such as
+/// `-{foobar}-` are activated, while converters must be rebuilt for every invocation if there are
+/// global rules. Check the project's README for more info.
+/// 
+/// Supported target variants: zh, zh-Hant, zh-Hans, zh-TW, zh-HK, zh-MO, zh-CN, zh-SG, zh-MY.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-#[pyfunction]
-fn zhconv(text: &str, target: &str, mediawiki: Option<bool>) -> String {
-    let target = Variant::from_str(target).expect("Unsupported target variant");
-    let mediawiki = mediawiki.unwrap_or(false);
-    if mediawiki {
-        zhconv_mw(text, target)
-    } else {
-        zhconv_plain(text, target)
-    }
-}
-
-/// zhconv with GIL released when converting. It allows in-parallel conversion when used together
-/// with multithreading. But be noted that it may incur non-trivial overhead due to extra FFI
-/// calls for small workloads.
-#[pyfunction]
-fn zhconv_nogil(py: Python<'_>, text: &str, target: &str, mediawiki: Option<bool>) -> String {
+fn zhconv(py: Python<'_>, text: &str, target: &str, mediawiki: Option<bool>) -> String {
     py.allow_threads(move || {
-        zhconv(text, target, mediawiki)
+        let target = Variant::from_str(target).expect("Unsupported target variant");
+        let mediawiki = mediawiki.unwrap_or(false);
+        if mediawiki {
+            zhconv_mw(text, target)
+        } else {
+            zhconv_plain(text, target)
+        }
     })
 }
 
-/// A Python module implemented in Rust.
+/// zhconv as in MediaWiki, oxidized with much more efficiency.
 #[pymodule]
 fn zhconv_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(crate::zhconv, m)?)?;
-    m.add_function(wrap_pyfunction!(zhconv_nogil, m)?)?;
     Ok(())
 }
