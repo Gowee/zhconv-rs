f099a2555bcfe1af07ec37eb164cb027bf2ce350
Hung-I Wang <whygowe@gmail.com>
2024-11-26 10:17:43 +0800

    bump CI

--- .cloudflare/setup-pages-env.sh
+++ .cloudflare/setup-pages-env.sh
@@ -6,7 +6,7 @@ curl -o rustup.sh https://sh.rustup.rs
 chmod +x rustup.sh
 ./rustup.sh -y --default-toolchain 1.81.0 # blocked by https://github.com/rustwasm/wasm-bindgen/issues/4211
 
-source $HOME/.cargo/env
+. $HOME/.cargo/env
 curl -o wasm-init.sh https://rustwasm.github.io/wasm-pack/installer/init.sh
 chmod +x wasm-init.sh
 ./wasm-init.sh
